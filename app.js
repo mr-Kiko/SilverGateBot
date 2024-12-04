54c00e4f8da1e326225172e2d2daefa32442f07d
kiarash abdollahi <kiarash35kia@gmail.com>
2024-12-04 18:14:10 +0330

    kk

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ const axios = require('axios');
 const botToken = '7922421871:AAGOql7sR7bI8fQ-qdKpYgj0fD2dA7TkPBI';  // توکن ربات شما
 const groupId = '-4173284707';  // شناسه گروه شما
 
-// تابع برای ارسال پیام
-async function sendMessage() {
+// بررسی اطلاعات گروه
+async function getGroupInfo() {
   try {
-    const response = await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-      chat_id: groupId,
-      text: 'سلام'
+    const response = await axios.post(`https://api.telegram.org/bot${botToken}/getChat`, {
+      chat_id: groupId
     });
-    console.log('پیام ارسال شد:', response.data);
+    console.log('اطلاعات گروه:', response.data);
   } catch (error) {
-    console.error('خطا در ارسال پیام:', error);
+    console.error('خطا در دریافت اطلاعات گروه:', error);
   }
 }
 
-// ارسال پیام هر 5 ثانیه
-setInterval(sendMessage, 5000);  // 5000 میلی‌ثانیه معادل 5 ثانیه
+getGroupInfo();
