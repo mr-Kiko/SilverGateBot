3c47f036edc66358d2a3ab90e4d04be7244033bd
kiarash abdollahi <kiarash35kia@gmail.com>
2024-12-04 18:06:59 +0330

    نن

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const axios = require('axios');
-
-const TELEGRAM_TOKEN = '7922421871:AAGOql7sR7bI8fQ-qdKpYgj0fD2dA7TkPBI'; // توکن ربات
+const botToken = '7922421871:AAGOql7sR7bI8fQ-qdKpYgj0fD2dA7TkPBI';
 
 async function getUpdates() {
-  const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/getUpdates`;
-
   try {
-    const response = await axios.get(url);
-    const updates = response.data;
-
-    console.log('آپدیت‌های دریافتی:', JSON.stringify(updates, null, 2));
+    const response = await axios.get(`https://api.telegram.org/bot${botToken}/getUpdates`);
+    const updates = response.data.result;
+    
+    updates.forEach(update => {
+      if (update.message && update.message.chat.type !== 'private') {  // فیلتر کردن پیام‌های گروه
+        console.log(update.message.chat.id);  // نمایش شناسه گروه
+        console.log(update.message.text);     // نمایش متن پیام
+      }
+    });
   } catch (error) {
-    console.error('خطا در دریافت آپدیت‌ها:', error);
+    console.error('Error getting updates:', error);
   }
 }
 
